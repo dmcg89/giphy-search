85b7d7780d771fefc5abb72532c39601f6231938
Drew McGowan <michael.mcgowan@students.makeschool.com>
2018-09-04 21:48:56 -0700

    better formating

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ var express = require('express');
 var app = express();
 var exphbs  = require('express-handlebars');
 var giphy = require('giphy-api')();
+var http = require('http');
 
 app.use(express.static('public'));
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+
 app.set('view engine', 'handlebars');
 
-
-var http = require('http');
 
 app.get('/', function (req, res) {
   let defaultTerm = req.query.term || "trending";
@@ -19,8 +19,19 @@ app.get('/', function (req, res) {
   });
 });
 
+app.get('/hello-gif', function (req, res) {
+  var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
+  res.render('hello-gif', {gifUrl: gifUrl})
+})
 
+app.get('/greetings/:name', function (req, res) {
+  var name = req.params.name;
+  res.render('greetings', {name: name});
+})
 
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
 
 // app.get('/', function (req, res) {
 //   console.log(req.query.term)
@@ -49,17 +60,3 @@ app.get('/', function (req, res) {
 //     });
 //   });
 // })
-
-app.get('/hello-gif', function (req, res) {
-  var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
-  res.render('hello-gif', {gifUrl: gifUrl})
-})
-
-app.get('/greetings/:name', function (req, res) {
-  var name = req.params.name;
-  res.render('greetings', {name: name});
-})
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
